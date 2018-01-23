636842385c58abfb693f7715a149956acf04e7af
greenRose2018 <brifun13@gmail.com>
2018-01-23 08:54:58 -0700

    created runner and controller with header

---
+++ FirstCPlusPlus/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  FirstCPlusPlus
+//
+//  Created by Morales, Brittney on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
---
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Morales, Brittney on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <stdio.h>
