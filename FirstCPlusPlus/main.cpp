045d2f5fa2f246419b6152d96d7dcc65082b54c7
greenRose2018 <brifun13@gmail.com>
2018-01-23 08:20:25 -0700

    startup

---
+++ FirstCPlusPlus/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Morales, Brittney on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
